20269ac2034e1565958e8a43fed364e75a132909
Darren Green <darrensgreen@gmail.com>
2016-10-09 20:49:16 -0500

    Entry point for the primes module
    Entry point for the primes module

---
+++ src/primes/index.jsx
@@ -0,0 +1,11 @@
+import acts from './actions'
+import cons from './constants'
+import primes from './components/Primes'
+import red from './reducers'
+
+export const actions = acts
+export const constants = cons
+export const Primes = primes
+export const reducers = red
+
+export default { actions, constants, Primes, reducers }
